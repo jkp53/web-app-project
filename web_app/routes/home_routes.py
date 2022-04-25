d7d637981e8dd10a273cdc45e5e0df8eaa771ca9
jkp53 <80708001+jkp53@users.noreply.github.com>
2022-04-25 17:36:19 -0400

    deleted some unnecessary files

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -5,33 +5,22 @@ from flask import Blueprint, request, render_template
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
-@home_routes.route("/home")
-def index():
-    print("HOME...")
-    #return "Welcome Home"
-    return render_template("home.html")
-
-@home_routes.route("/about")
-def about():
-    print("ABOUT...")
-    #return "About Me"
-    return render_template("about.html")
-
-@home_routes.route("/another")
-def another():
-    print("ANOTHER PAGE MAYBE...")
-    return "Here is another page"
-
 @home_routes.route("/homepage")
-def homepage():
+def index():
     print("You reached the homepage...")
     locations = [
         {"location_id":"1", "location_name":"Five Spice", "location_hours":"hours test", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_5Spice.png&w=900&h=508&r=1"},
-        {"location_id":"2", "location_name":"Olive Branch", "location_hours":"hours test", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_OliveBranch.png&w=900&h=508&r=1"}
+        {"location_id":"2", "location_name":"Olive Branch", "location_hours":"Monday – Thursday 11:00am – 8:00pm; Friday 11:00am – 4:00pm; Sunday 4:00pm – 8:00pm", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_OliveBranch.png&w=900&h=508&r=1"},
+        {"location_id":"3", "location_name":"Whisk", "location_hours":"hours test", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_Whisk.png&w=900&h=508&r=1"}
         ]
     print(type(locations))
     return render_template("homepage.html", locations=locations)
 
+
+@home_routes.route("/leaveareview")
+def reviewpage():
+    print("You reached the review page...")
+    return render_template("review.html")
 
 @home_routes.route("/bootstraptest")
 def bootstraptest():
@@ -41,17 +30,3 @@ def bootstraptest():
     # web_app/routes/home_routes.py
 
 # ...
-
-@home_routes.route("/hello")
-def hello_world():
-    print("HELLO...", dict(request.args))
-    # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
-    # ... which will return None instead of throwing an error if key is not present
-    # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
-
-    #go check the url params for one called "name", and use it if possible
-    #if no name parameter is specified, use a default value
-    name = request.args.get("name") or "World"
-    message = f"Hello, {name}!"
-    #return message
-    return render_template("hello.html", message=message, other="YOLO")
