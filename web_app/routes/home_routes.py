2cb978a794f22fe56864947e28dc12682f9f7f8c
jkp53 <80708001+jkp53@users.noreply.github.com>
2022-04-25 18:49:56 -0400

    added url params

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -13,13 +13,15 @@ def index():
         {"location_id":"2", "location_name":"Olive Branch", "location_hours":"Monday – Thursday 11:00am – 8:00pm; Friday 11:00am – 4:00pm; Sunday 4:00pm – 8:00pm", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_OliveBranch.png&w=900&h=508&r=1"},
         {"location_id":"3", "location_name":"Whisk", "location_hours":"hours test", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_Whisk.png&w=900&h=508&r=1"}
         ]
+
     print(type(locations))
     return render_template("homepage.html", locations=locations)
 
-
-@home_routes.route("/leaveareview")
-def reviewpage():
+@home_routes.route("/leaveareview/<int:location_id>")
+#@home_routes.route("/leaveareview")
+def reviewpage(location_id):
     print("You reached the review page...")
+    print("You chose" + str(location_id))
     return render_template("review.html")
 
 @home_routes.route("/bootstraptest")
