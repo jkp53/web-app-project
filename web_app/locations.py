707e94294e613789f8b36a5d8f2f21b2120858a5
jkp53 <80708001+jkp53@users.noreply.github.com>
2022-04-26 17:59:40 -0400

    enabled reading of loactions from google sheet

---
+++ web_app/locations.py
@@ -0,0 +1,19 @@
+# Reading csv from google sheets
+
+# Code adapted from https://www.youtube.com/watch?v=cnPlKLEGR7E
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('web_app/gu-dining-locations-cf538c00daae.json', scope)
+
+client = gspread.authorize(creds)
+
+workbook1 = client.open("Georgetown Dining Locations Data").sheet1
+
+locations = workbook1.get_all_records()
+
+print(type(locations))
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,12 +1,7 @@
 # web_app/routes/home_routes.py
 
 from flask import Blueprint, request, render_template
-
-locations = [
-    {"location_id":"1", "location_name":"Five Spice", "location_hours":"hours test", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_5Spice.png&w=900&h=508&r=1"},
-    {"location_id":"2", "location_name":"Olive Branch", "location_hours":"Monday – Thursday 11:00am – 8:00pm; Friday 11:00am – 4:00pm; Sunday 4:00pm – 8:00pm", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_OliveBranch.png&w=900&h=508&r=1"},
-    {"location_id":"3", "location_name":"Whisk", "location_hours":"hours test", "location_logo_url":"https://cms.concept3d.com/map/lib/image-cache/i.php?mapId=999&image=999/Logos-bgTransparent_Whisk.png&w=900&h=508&r=1"}
-    ]
+from web_app.locations import locations
 
 home_routes = Blueprint("home_routes", __name__)
 
